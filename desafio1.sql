744be8026dd43c19e89d194fcd66cb892a13da31
Thais Cardoso <thaistauane.cardoso@gmail.com>
2022-05-09 23:24:06 -0300

    corrigindo requisito 1

--- desafio1.sql
+++ desafio1.sql
@@ -5,7 +5,7 @@ CREATE DATABASE SpotifyClone;
 CREATE TABLE SpotifyClone.plan(
     plan_id INT PRIMARY kEY AUTO_INCREMENT,
     type_plan VARCHAR(20) NOT NULL,
-    plan_price DECIMAL(5,2) NOT NULL,
+    plan_price DECIMAL(3,2) NOT NULL
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.users(
@@ -14,25 +14,7 @@ CREATE TABLE SpotifyClone.users(
     age INT  NOT NULL, 
     plan_id INT NOT NULL,
     date_subscription DATE NOT NULL,
-    FOREIGN KEY (plan_id) REFERENCES SpotifyClone.plan(plan_id)
-) engine = InnoDB;
-
-CREATE TABLE SpotifyClone.historic(
-    historic_id INT PRIMARY kEY AUTO_INCREMENT,
-    users_id INT NOT NULL,
-    songs_id INT NOT NULL,
-    data_reproduction DATE NOT NULL,
-    CONSTRAINT PRIMARY KEY (historic_id, users_id, songs_id),
-    FOREIGN KEY (users_id) REFERENCES SpotifyClone.users(users_id),
-    FOREIGN KEY (songs_id) REFERENCES SpotifyClone.songs(songs_id)
-) engine = InnoDB;
-
-CREATE TABLE SpotifyClone.album(
-    album_id INT PRIMARY kEY AUTO_INCREMENT,
-    artist_id INT NOT NULL,
-    name_album VARCHAR(40) NOT NULL,
-    year_release YEAR NOT NULL,
-    FOREIGN KEY (artist_id) REFERENCES SpotifyClone.artist(artist_id)
+    FOREIGN KEY (plan_id) REFERENCES plan(plan_id)
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.artist(
@@ -40,29 +22,47 @@ CREATE TABLE SpotifyClone.artist(
     name_artist VARCHAR(50) NOT NULL
 ) engine = InnoDB;
 
+CREATE TABLE SpotifyClone.album(
+    album_id INT PRIMARY kEY AUTO_INCREMENT,
+    artist_id INT NOT NULL,
+    name_album VARCHAR(40) NOT NULL,
+    year_release YEAR NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
+) engine = InnoDB;
+
 CREATE TABLE SpotifyClone.songs(
     id INT PRIMARY kEY AUTO_INCREMENT,
     album_id INT NOT NULL,
     name_songs VARCHAR(100) NOT NULL,
     duration_seconds INT NOT NULL,
-    FOREIGN KEY (album_id) REFERENCES SpotifyClone.album(album_id)
+    FOREIGN KEY (album_id) REFERENCES album(album_id)
+) engine = InnoDB;
+
+CREATE TABLE SpotifyClone.historic(
+    historic_id INT AUTO_INCREMENT,
+    users_id INT NOT NULL,
+    songs_id INT NOT NULL,
+    data_reproduction DATETIME NOT NULL,
+    CONSTRAINT PRIMARY KEY (historic_id, users_id, songs_id),
+    FOREIGN KEY (users_id) REFERENCES users(users_id),
+    FOREIGN KEY (songs_id) REFERENCES songs(id)
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.following(
-    id INT PRIMARY kEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     follow INT NOT NULL,
     artist_id INT NOT NULL,
     CONSTRAINT PRIMARY KEY (id, follow, artist_id),
-    FOREIGN KEY (follow) REFERENCES SpotifyClone.users(users_id),
-    FOREIGN KEY (artist_id) REFERENCES SpotifyClone.artist(artist_id)
+    FOREIGN KEY (follow) REFERENCES users(users_id),
+    FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
 ) engine = InnoDB;
 
 INSERT INTO SpotifyClone.plan (type_plan, plan_price)
 VALUES
-  ('gratuito', 0),
-  ('familiar', 7,99),
-  ('pessoal', 6,99),
-  ('universitário', 5,99);
+  ('gratuito', 0.00),
+  ('familiar', 7.99),
+  ('pessoal', 6.99),
+  ('universitário', 5.99);
 
 INSERT INTO SpotifyClone.users (full_name, age, plan_id, date_subscription)
 VALUES
@@ -77,91 +77,6 @@ VALUES
   ('Angelina', 42, 2, '2018-04-29'),
   ('Paul', 46, 2, '2017-01-17');
 
-INSERT INTO SpotifyClone.historic (users_id, songs_id, data_reproduction)
-VALUES
-  (1, 36, '2020-02-28 10:45:55'),
-  (1, 25, '2020-05-02 05:30:35'),
-  (1, 23, '2020-03-06 11:22:33'),
-  (1, 14, '2020-08-05 08:05:17'),
-  (1, 15, '2020-09-14 16:32:22'),
-  (2, 34, '2020-01-02 07:40:33'),
-  (2, 24, '2020-05-16 06:16:22'),
-  (2, 21, '2020-10-09 12:27:48'),
-  (2, 39, '2020-09-21 13:14:46'),
-  (3, 6 , '2020-11-13 16:55:13'),
-  (3, 3 , '2020-12-05 18:38:30'),
-  (3, 26, '2020-07-30 10:00:00'),
-  (4, 2 , '2021-08-15 17:10:10'),
-  (4, 35, '2021-07-10 15:20:30'),
-  (4, 27, '2021-01-09 01:44:33'),
-  (5, 7 , '2020-07-03 19:33:28'),
-  (5, 12, '2017-02-24 21:14:22'),
-  (5, 14, '2020-08-06 15:23:43'),
-  (5, 1 , '2020-11-10 13:52:27'),
-  (6, 38, '2019-02-07 20:33:48'),
-  (6, 29, '2017-01-24 00:31:17'),
-  (6, 30, '2017-10-12 12:35:20'),
-  (6, 22, '2018-05-29 14:56:41'),
-  (7, 5 , '2018-05-09 22:30:49'),
-  (7, 4 , '2020-07-27 12:52:58'),
-  (7, 11, '2018-01-16 18:40:43'),
-  (8, 39, '2018-03-21 16:56:40'),
-  (8, 40, '2020-10-18 13:38:05'),
-  (8, 32, '2019-05-25 08:14:03'),
-  (8, 33, '2021-08-15 21:37:09'),
-  (9, 16, '2021-05-24 17:23:45'),
-  (9, 17, '2018-12-07 22:48:52'),
-  (9, 9 , '2020-04-01 03:36:00'),
-  (9, 8 , '2021-03-14 06:14:26'),
-  (10, 20, '2017-02-06 08:21:34'),
-  (10, 21, '2017-12-04 05:33:43'),
-  (10, 12, '2017-07-27 05:24:49'),
-  (10, 13, '2017-12-25 01:03:57');
-
-
-INSERT INTO SpotifyClone.album (artist_id, name_album, year_release)
-VALUES
-  (1,	'Envious', 1990),
-  (1,	'Envious', 1990),
-  (1,	'Envious', 1990),
-  (1,	'Exuberant', 1993),
-  (1,	'Exuberant', 1993),
-  (2,	'Hallowed Steam', 1995),
-  (2,	'Hallowed Steam', 1995),
-  (2,	'Hallowed Steam', 1995),
-  (2,	'Hallowed Steam', 1995),
-  (3,	'Incandescent',	1998),
-  (3,	'Incandescent',	1998),
-  (3,	'Incandescent',	1998),
-  (3,	'Incandescent',	1998),
-  (3,	'Incandescent',	1998),
-  (3,	'Incandescent',	1998),
-  (4,	'Temporary Culture', 2001),
-  (4,	'Temporary Culture', 2001),
-  (4,	'Temporary Culture', 2001),
-  (4,	'Library of liberty', 2003),
-  (4,	'Library of liberty', 2003),
-  (4,	'Library of liberty', 2003),
-  (4,	'Library of liberty', 2003),
-  (4,	'Library of liberty', 2003),
-  (5,	'Chained Down', 2007),
-  (5,	'Chained Down', 2007),
-  (5,	'Chained Down', 2007),
-  (5,	'Chained Down', 2007),
-  (5,	'Cabinet of fools',	2012),
-  (5,	'Cabinet of fools',	2012),
-  (5,	'Cabinet of fools',	2012),
-  (5,	'Cabinet of fools',	2012),
-  (5,	'No guarantees', 2015),
-  (5,	'No guarantees', 2015),
-  (5,	'No guarantees', 2015),
-  (5,	'No guarantees', 2015),
-  (6,	'Apparatus', 2015),
-  (6,	'Apparatus', 2015),
-  (6,	'Apparatus', 2015),
-  (6,	'Apparatus', 2015),
-  (6,	'Apparatus', 2015);
-
 INSERT INTO SpotifyClone.artist (name_artist)
 VALUES
   ('Walter Phoenix'),
@@ -170,6 +85,20 @@ VALUES
   ('Freedie Shannon'),
   ('Tyler Isle'),
   ('Fog');  
+
+INSERT INTO SpotifyClone.album (artist_id, name_album, year_release)
+VALUES
+  (1,	'Envious', 1990),
+  (1,	'Exuberant', 1993),
+  (2,	'Hallowed Steam', 1995),
+  (3,	'Incandescent',	1998),
+  (4,	'Temporary Culture', 2001),
+  (4,	'Library of liberty', 2003),
+  (5,	'Chained Down', 2007),
+  (5,	'Cabinet of fools',	2012),
+  (5,	'No guarantees', 2015),
+  (6,	'Apparatus', 2015);
+
 
 INSERT INTO SpotifyClone.songs (album_id, name_songs, duration_seconds)
 VALUES
@@ -213,6 +142,47 @@ VALUES
    (10, "Wouldn't It Be Nice",	213),
    (10, "Baby", 136),
    (10, "You Make Me Feel So..", 83);
+
+INSERT INTO SpotifyClone.historic (users_id, songs_id, data_reproduction)
+VALUES
+  (1, 36, '2020-02-28 10:45:55'),
+  (1, 25, '2020-05-02 05:30:35'),
+  (1, 23, '2020-03-06 11:22:33'),
+  (1, 14, '2020-08-05 08:05:17'),
+  (1, 15, '2020-09-14 16:32:22'),
+  (2, 34, '2020-01-02 07:40:33'),
+  (2, 24, '2020-05-16 06:16:22'),
+  (2, 21, '2020-10-09 12:27:48'),
+  (2, 39, '2020-09-21 13:14:46'),
+  (3, 6 , '2020-11-13 16:55:13'),
+  (3, 3 , '2020-12-05 18:38:30'),
+  (3, 26, '2020-07-30 10:00:00'),
+  (4, 2 , '2021-08-15 17:10:10'),
+  (4, 35, '2021-07-10 15:20:30'),
+  (4, 27, '2021-01-09 01:44:33'),
+  (5, 7 , '2020-07-03 19:33:28'),
+  (5, 12, '2017-02-24 21:14:22'),
+  (5, 14, '2020-08-06 15:23:43'),
+  (5, 1 , '2020-11-10 13:52:27'),
+  (6, 38, '2019-02-07 20:33:48'),
+  (6, 29, '2017-01-24 00:31:17'),
+  (6, 30, '2017-10-12 12:35:20'),
+  (6, 22, '2018-05-29 14:56:41'),
+  (7, 5 , '2018-05-09 22:30:49'),
+  (7, 4 , '2020-07-27 12:52:58'),
+  (7, 11, '2018-01-16 18:40:43'),
+  (8, 39, '2018-03-21 16:56:40'),
+  (8, 40, '2020-10-18 13:38:05'),
+  (8, 32, '2019-05-25 08:14:03'),
+  (8, 33, '2021-08-15 21:37:09'),
+  (9, 16, '2021-05-24 17:23:45'),
+  (9, 17, '2018-12-07 22:48:52'),
+  (9, 9 , '2020-04-01 03:36:00'),
+  (9, 8 , '2021-03-14 06:14:26'),
+  (10, 20, '2017-02-06 08:21:34'),
+  (10, 21, '2017-12-04 05:33:43'),
+  (10, 12, '2017-07-27 05:24:49'),
+  (10, 13, '2017-12-25 01:03:57');
 
 INSERT INTO SpotifyClone.following (follow, artist_id)
 VALUES
---
+++ jest.config.js 
@@ -0,0 +1,4 @@
+const config = {
+  testTimeout: 50000,
+};
+module.exports = config;
